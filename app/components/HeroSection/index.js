524ff55685c1359907e17b4b55fac2482c4bd439
Lucky-Wangari <wangarimarylucky@gmail.com>
2025-01-27 20:37:42 +0300

    Herosection

--- app/components/HeroSection/index.js
+++ app/components/HeroSection/index.js
@@ -2,14 +2,6 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <div className="flex items-center space-x-3">
-    <Image
-      src="/images/chickens.jpg" // Add this image to public/images
-      alt="Farmers Fresh Feeds Logo"
-      width={50}
-      height={50}
-    />
-    <span className="text-xl font-bold">Farmers Fresh Feeds</span>
-  </div>
+    
   );
 }
